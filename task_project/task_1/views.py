eea2c0fe956e3a1c3780078745519b25bcb0fd5b
Vitaliy <balu83vs@gmail.com>
2023-10-17 22:52:23 +0300

    task_1_done

--- task_project/task_1/views.py
+++ task_project/task_1/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from load_file import read_file
+from upload_file import write_file
 
 # Create your views here.
+
+def indexpage(request):
+    return render(request, template_name='index.html')
+
+
+def word_listpage(request):
+    words_list = read_file()
+    return render(request, template_name='words_list.html', context={'words_list': words_list})
+
+
+def add_wordpage(request):
+    if request.method == 'GET':
+        return render(request, template_name='add_word.html')
+    else:
+        write_file(request.POST['word1'], request.POST['word2'])
+        return redirect(add_wordpage)
