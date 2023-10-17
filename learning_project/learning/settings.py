bd3bb7c44d51f7db85daf9c9f28dd7c18f3afb64
Vitaliy <balu83vs@gmail.com>
2023-10-17 23:05:06 +0300

    lesson_1-done

--- learning_project/learning/settings.py
+++ learning_project/learning/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lesson_1',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
--- learning_project/lesson_1/views.py
+++ learning_project/lesson_1/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+posts = [
+    {
+        'Title': 'Test_title',
+        'Description': 'Test_description',
+        'Author': 'Test_author',
+        'Date': 'Test_date'
+    }
+]
+
+def indexpage(request):
+    return render(request, template_name='index.html', context={'articles': posts, 'page': 'index'})
+
+def aboutpage(request):
+    return render(request, template_name='about.html', context={'page': 'about'})
+
+def contactpage(request):
+    if request.method == 'GET':
+        return render(request, template_name='contact.html', context={'page': 'contact'})
+    else:
+        with open("D:\py_learning\learning_django\django_stepik\stepik_django_test_1\mainapp\message_result.txt", "a") as message_file:
+            message_file.writelines(f'{request.POST["name"]}, {request.POST["email"]}, {request.POST["subject"]}, {request.POST["message"]}\n')
+        return redirect(contactpage)
