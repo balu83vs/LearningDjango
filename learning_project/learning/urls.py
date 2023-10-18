34a61475b731b37fda34289fd04832291f41a7f4
Vitaliy <balu83vs@gmail.com>
2023-10-18 11:35:49 +0300

    lesson_2 - DB-Ok, models_Ok, ready for change views

--- learning_project/learning/urls.py
+++ learning_project/learning/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('lesson_1.urls'))
+    path('', include('lesson_2.urls'))
 ]
--- learning_project/lesson_2/models.py
+++ learning_project/lesson_2/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    Title = models.CharField(max_length=50)
+    Description = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.CharField(max_length = 50)
+    Categories = models.CharField(max_length=50)
+    Date = models.DateField()
+
+class Comments(models.Model):
+    Author = models.CharField(max_length=50)
+    Email = models.EmailField()
+    Message = models.TextField()
+    Article = models.ForeignKey(Articles, on_delete=models.CASCADE)
+
+
